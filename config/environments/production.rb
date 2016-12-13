2321e28382a8eb45bb17a02f7c98c6d1f4b45f6e
Laura Reynolds <lr.rnlds@gmail.com>
2016-12-13 15:56:13 -0800

    AWS creds change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('pintravel'),
+      bucket: 'pintravel',
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('Oregon'),
+      s3_region: 'Oregon',
     }
   }
 end
