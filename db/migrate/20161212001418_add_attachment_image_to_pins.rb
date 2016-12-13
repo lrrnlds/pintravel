cd10851bc3856a439062dfe1882dd59542502bb6
Laura Reynolds <lr.rnlds@gmail.com>
2016-12-13 15:48:23 -0800

    migration change for paperclip s3 deployment

--- db/migrate/20161212001418_add_attachment_image_to_pins.rb
+++ db/migrate/20161212001418_add_attachment_image_to_pins.rb
@@ -1,7 +1,7 @@
 class AddAttachmentImageToPins < ActiveRecord::Migration
   def self.up
     change_table :pins do |t|
-      t.attachment :image
+      add_attachment :pins, :image
     end
   end
 
