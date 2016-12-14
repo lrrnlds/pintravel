5feeaa5f9d56123134382eae3077ff96cc26d06f
Laura Reynolds <lr.rnlds@gmail.com>
2016-12-13 16:30:03 -0800

    Fix for broken img link

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,6 @@ class Pin < ApplicationRecord
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "300 x 300>" }
+  # :url => '/:pin/:attachment/:id/:style_:basename.:extension'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
